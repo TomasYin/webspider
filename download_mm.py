428da2840e1f83e1b8f3ee9fe603e3de5be856f4
TomasYin <43948117+TomasYin@users.noreply.github.com>
2018-11-01 02:02:45 +0800

    download_mm new

--- download_mm.py
+++ download_mm.py
@@ -3,11 +3,20 @@
 
 import urllib.request
 import os
+import random
 
 def url_open(url):
     req = urllib.request.Request(url)
     req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36')
-    response = urllib.request.urlopen(req)
+    
+    proxies = ['61.135.217.7:80','218.14.115.211:3128','122.114.31.177:808','121.196.218.197:3128','14.118.252.59:6666','106.56.102.163:8070','222.171.83.213:63000','14.118.252.202:6666']
+    proxy = random.choice(proxies)
+    
+    proxy_support = urllib.request.ProxyHandler({'http':proxy})
+    opener = urllib.request.build_opener(proxy_support)
+    
+    
+    response = opener.open(req)
     html = response.read().decode('utf-8')
     html = response.read()
     return html
